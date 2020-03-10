1dfa4186960e24010fa29a21587b97a0a9c8672a
PRASAD BHAMRE <prasadbhamre@PRASADs-MacBook-Pro.local>
2020-03-10 17:06:09 +0530

    changes in app.component.html

--- projects/envApp/src/app/app.component.ts
+++ projects/envApp/src/app/app.component.ts
@@ -8,12 +8,14 @@ import {HttpClient} from '@angular/common/http';
 })
 export class AppComponent {
   title = 'envApplication';
+  apiUrl :string;
   constructor(private http: HttpClient,private envService:EnvironmentService){
     this.dataRes();
   }
 
   public dataRes():void{
-    this.http.get(`${this.envService.envConfig.apiUrl}employees`).subscribe((res)=>{
+    this.apiUrl = this.envService.envConfig.apiUrl;
+    this.http.get(`${this.apiUrl}employees`).subscribe((res)=>{
       const resData =res;
     },(error)=>console.log(error));
   }
